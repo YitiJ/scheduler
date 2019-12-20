1a6f0744486d59658b5400aae21dbc6024e945d0
Jenny Lei <jenny.h.lei@hotmail.com>
2019-12-19 20:41:54 -0500

    added blank calendar screen

---
+++ lib/screens/calendarScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:scheduler/customTemplates/colours.dart';
+
+import 'package:scheduler/customTemplates/customWidgets.dart';
+
+class CalendarScreen extends StatelessWidget {
+  CalendarScreen({Key key}) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
