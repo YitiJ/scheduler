e6afe79a07ab3b91150a85f909070337b175042d
Jenny Lei <jenny.h.lei@hotmail.com>
2019-12-21 02:03:41 -0500

    back btn navigates back to calendar

--- lib/screens/scheduleScreen.dart
+++ lib/screens/scheduleScreen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+
 import 'package:scheduler/data/models.dart';
 import 'package:scheduler/customTemplates/themes.dart';
 import 'package:scheduler/customTemplates/colours.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:scheduler/bloc/navBar/navbar.dart';
 
 import 'package:intl/intl.dart';
 
@@ -32,27 +36,59 @@ class ScheduleScreen extends StatelessWidget {
 Widget _headerNav() {
   return Stack(
     children: <Widget>[
-      FlatButton(
-        child: Row(
-          children: [
-            Icon(
-              Icons.arrow_left,
-              color: Colors.white,
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5.0),
-              child: Text(
-                'BACK',
-                style: mainTheme.textTheme.body1,
-              ),
-            ),
-          ],
-        ),
+      Positioned(
+        left: 0,
+        child: _BackBtn(),
       ),
-
       SegmentedControl(),
     ],
   );
+}
+
+class BackBtnBloc extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NavBarBloc, NavBarState>(
+      condition: (previousState, state) =>
+          state.runtimeType != previousState.runtimeType,
+      builder: (context, state) => _BackBtn(),
+    );
+  }
+}
+    
+class _BackBtn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _mapStateToActionButtons(
+      navBarBloc: BlocProvider.of<NavBarBloc>(context));
+  }
+
+  Widget _mapStateToActionButtons({
+    NavBarBloc navBarBloc,
+  }) {
+    return _btn( navBarBloc );
+  }
+
+  Widget _btn(NavBarBloc navBarBloc) {  
+    return FlatButton(
+      child: Row(
+        children: [
+          Icon(
+            Icons.arrow_left,
+            color: Colors.white,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 5.0),
+            child: Text(
+              'BACK',
+              style: mainTheme.textTheme.body1,
+            ),
+          ),
+        ],
+      ),
+      onPressed: () => navBarBloc.add(CalendarEvent()),
+    );
+  }
 }
 
 class SegmentedControl extends StatelessWidget {
