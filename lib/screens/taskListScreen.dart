a4285e6fa1deef8aa9e7e772ded54ca96716c1b3
Jenny Lei <jenny.h.lei@hotmail.com>
2019-12-21 12:00:15 -0500

    fixed nav issues with task list screen

--- lib/screens/taskListScreen.dart
+++ lib/screens/taskListScreen.dart
@@ -9,39 +9,42 @@ import 'package:scheduler/data/dbManager.dart';
 class TaskListScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return BlocProvider(
-      create: (context) => TaskBloc(dbManager: DbManager.instance)..add(LoadTask()),
-      child:BlocBuilder<TaskBloc, TaskState>(
-        builder: (context, state){
-          List<List<Widget>> contents = new List<List<Widget>>();
-          Widget content;
-          if(state is TaskLoading){
-            content = Card(color: Colors.white,);
+    return Container(
+      child: BlocProvider(
+        create: (context) => TaskBloc(dbManager: DbManager.instance)..add(LoadTask()),
+        child:BlocBuilder<TaskBloc, TaskState>(
+          builder: (context, state){
+            List<List<Widget>> contents = new List<List<Widget>>();
+            Widget content;
+            if(state is TaskLoading){
+              content = Card(color: Colors.white,);
+            }
+            else if (state is TaskLoaded){
+              state.tasks.forEach(
+                  (task) => contents.add(<Widget>[
+                    Text(task.name),
+                    Text((task.description == null) ? "" : task.description)])
+                );
+              content = CustomList(contents);
+            }
+            else if (state is TaskNotLoaded){
+              content = Container(height: 0.00, width: 0.00,);
+            }
+            return Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "Saved Task",
+                  style: Theme.of(context).textTheme.body1,
+                )),
+                Expanded(child: content),
+            ]
+            );
           }
-          else if (state is TaskLoaded){
-            state.tasks.forEach(
-                (task) => contents.add(<Widget>[
-                  Text(task.name),
-                  Text((task.description == null) ? "" : task.description)])
-              );
-            content = CustomList(contents);
-          }
-          else if (state is TaskNotLoaded){
-            content = Container(height: 0.00, width: 0.00,);
-          }
-          return Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "Saved Task",
-                style: Theme.of(context).textTheme.body1,
-              )),
-              Expanded(child: content),
-          ]
-          );
-        }
-      ));
+        ),
+      ),
+    );
   }
 
 }
