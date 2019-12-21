ec9cbe2f8588ccf05f0d8c0dfdc3c8a260c0c6bf
Jenny Lei <jenny.h.lei@hotmail.com>
2019-12-21 18:50:22 -0500

    fixed bottom nav bar bloc to implement streams

--- lib/bloc/navBar/navbar.dart
+++
@@ -1,3 +0,0 @@
-export 'navBar_bloc.dart';
-export 'navBar_event.dart';
-export 'navBar_state.dart';
--- lib/customTemplates/navBar.dart
+++ lib/customTemplates/navBar.dart
@@ -4,9 +4,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:scheduler/customTemplates/colours.dart';
 
-import 'package:scheduler/bloc/navBar/navBar.dart';
+import 'package:scheduler/bloc/navBar/navbar_bloc.dart';
 
 class NavBar extends StatelessWidget {
+  NavBar({Key key, this.bloc}) : super (key: key);
+
+  final BottomNavBarBloc bloc;
+
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -16,72 +20,40 @@ class NavBar extends StatelessWidget {
         color: purple[200],
         padding: const EdgeInsets.only(top: 10, bottom: 15),
         
-        child: NavRow(),
+        child: navRow(bloc),
       ),
     );
   }
 }
 
-class NavRow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<NavBarBloc, NavBarState>(
-      condition: (previousState, state) =>
-          state.runtimeType != previousState.runtimeType,
-      builder: (context, state) => _Actions(),
-    );
-  }
+Widget navRow(BottomNavBarBloc _bloc) {
+  return StreamBuilder(
+    stream: _bloc.page,
+    builder: (context, snapshot) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: buttons(_bloc),
+      );
+    },
+  );
 }
 
-class _Actions extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: _mapStateToActionButtons(
-        navBarBloc: BlocProvider.of<NavBarBloc>(context),
-      ),
-    );
-  }
-
-  List<Widget> _mapStateToActionButtons({
-    NavBarBloc navBarBloc,
-  }) {
-    final NavBarState currentState = navBarBloc.state;
-    if (currentState is Timer) {
-      return buttons( navBarBloc );
-    }
-    if (currentState is Calendar) {
-      return buttons( navBarBloc );
-    }
-    if (currentState is Schedule) {
-      return buttons( navBarBloc );
-    }
-    if (currentState is TaskList) {
-      return buttons( navBarBloc );
-    }
-    return [];
-  }
-}
-
-List<Widget> buttons( NavBarBloc navBarbloc ) {
-  final NavBarBloc navBarBloc = navBarbloc;
-
+List<Widget> buttons(BottomNavBarBloc _bloc) {
   return [
       IconButton(
         highlightColor: purple,
         icon: new Icon(Icons.timer, color: Colors.white,),
-        onPressed: () => navBarBloc.add(TimerEvent()),
+        onPressed: () => _bloc.switchPage(Pages.timer),
       ),
       IconButton(
         highlightColor: purple,
         icon: new Icon(Icons.calendar_today, color: Colors.white,),
-        onPressed: () => navBarBloc.add(CalendarEvent()),
+        onPressed: () => _bloc.switchPage(Pages.calendar),
       ),
       IconButton(
         highlightColor: purple,
         icon: new Icon(Icons.save_alt, color: Colors.white,),
-        onPressed: () => navBarBloc.add(TaskListEvent()),
+        onPressed: () => _bloc.switchPage(Pages.taskList),
       ),
       IconButton(
         highlightColor: purple,
