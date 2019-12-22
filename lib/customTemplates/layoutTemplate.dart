0daa1645be1c374449da82cee4ed5dedd9d8f1b0
yiti_ <yitijiang@gmail.com>
2019-12-22 00:35:47 -0500

    extracted page widget

--- lib/customTemplates/layoutTemplate.dart
+++ lib/customTemplates/layoutTemplate.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scheduler/bloc/task/task.dart';
@@ -16,62 +18,109 @@ class LayoutTemplate extends StatefulWidget {
 }
 
 class _LayoutTemplateState extends State<LayoutTemplate> {
-  final BottomNavBarBloc _bottomNavBarBloc = BottomNavBarBloc();
+  final BottomNavBarBloc _bloc = BottomNavBarBloc();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       // backgroundColor: Colors.transparent,
-      body: Page(_bottomNavBarBloc),
+      body: StreamBuilder(
+        stream: _bloc.page,
+        builder: (context, snapshot) {
+          Widget screen;
+          switch(_bloc.getPage()) {
+            case Pages.timer:
+              screen = TimerScreen();
+              break;
+            case Pages.calendar:
+              screen = CalendarScreen(bloc: _bloc);
+              break;
+            case Pages.taskList:
+              screen = TaskListScreen();
+              break;
+            default:
+              screen = TimerScreen();
+            }
+          return getPageWidget(screen, _bloc);
+        },)
     );
   }
 }
-
-Widget Page(BottomNavBarBloc _bloc) {
+Widget getPageWidget(Widget screen, BottomNavBarBloc bloc){
   return Stack(
-    children: [
-      Positioned.fill(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [darkRed[700], orange[700]],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter),
-          ),
+  children: [
+    Positioned.fill(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [darkRed[700], orange[700]],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+        ),
 
-          child : Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
-                  child: StreamBuilder(
-                    stream: _bloc.page,
-                    builder: (context, snapshot) {
-                        final pages = [TimerScreen(), CalendarScreen(bloc: _bloc), ScheduleScreen(bloc: _bloc), TaskListScreen()];
-                        switch(_bloc.getPage()) {
-                          case Pages.timer:
-                            return pages[0];
-                          case Pages.calendar:
-                            return pages[1];
-                          case Pages.schedule:
-                            return pages[2];
-                          case Pages.taskList:
-                            return pages[3];
-                          default:
-                            return pages[0];
-                        }
-                      },
-                    ),
-                ),
+        child : Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+                child: screen,
               ),
-
-              NavBar(bloc: _bloc),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    ],
-  );
+    ),
+    (bloc != null) ? NavBar(bloc: bloc) : Container(height: 0.00, width: 0.00)
+  ],
+);
 }
+
+// Widget Page(BottomNavBarBloc _bloc) {
+//   return Stack(
+//     children: [
+//       Positioned.fill(
+//         child: Container(
+//           decoration: BoxDecoration(
+//             gradient: LinearGradient(
+//                 colors: [darkRed[700], orange[700]],
+//                 begin: Alignment.topCenter,
+//                 end: Alignment.bottomCenter),
+//           ),
+
+//           child : Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Expanded(
+//                 child: Padding(
+//                   padding: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+//                   child: StreamBuilder(
+//                     stream: _bloc.page,
+//                     builder: (context, snapshot) {
+//                         final pages = [TimerScreen(), CalendarScreen(bloc: _bloc), ScheduleScreen(bloc: _bloc), TaskListScreen()];
+//                         switch(_bloc.getPage()) {
+//                           case Pages.timer:
+//                             return pages[0];
+//                           case Pages.calendar:
+//                             return pages[1];
+//                           case Pages.schedule:
+//                             return pages[2];
+//                           case Pages.taskList:
+//                             return pages[3];
+//                           default:
+//                             return pages[0];
+//                         }
+//                       },
+//                     ),
+//                 ),
+//               ),
+
+//               NavBar(bloc: _bloc),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ],
+//   );
+// }
